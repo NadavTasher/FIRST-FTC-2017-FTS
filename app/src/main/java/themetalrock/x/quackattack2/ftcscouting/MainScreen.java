dc5045fa251c1ed43c4ee3c71f9079abea3fceae
nadav <tashernadav@gmail.com>
2017-10-09 03:21:22 -0400

    Changed theme and added Extras Libraries

---
+++ app/src/main/java/themetalrock/x/quackattack2/ftcscouting/MainScreen.java
@@ -0,0 +1,12 @@
+package themetalrock.x.quackattack2.ftcscouting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
